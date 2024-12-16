31f17c5e56344680fbafee49d674046045d8bb9f
Michael <michaellam1996@gmail.com>
2024-12-16 23:06:34 +0800

    added error messages

--- DocGen.py
+++ DocGen.py
@@ -5,27 +5,44 @@ from docx import Document
 from docxtpl import DocxTemplate
 import jinja2
 
-target_csv = "points.csv"
+# actual files to be used
+# target_csv = "points.csv"
+# target_template = "TnC template.docx"
 target_dir = "TnC forms"
+
+#for testing
+target_csv = "points_example.csv"
+target_template = "TnC template Sample.docx"
+
 
 # Read points.csv to gather information
 def read_csv(base_dir: Path):
-    assert (base_dir / 'points.csv').exists(), "points.csv does not exist" # Check if the file exists
     items, desc, info = [], [], {}
-    with open(base_dir / 'points.csv', "r", encoding='utf-8-sig', errors='ignore') as file:
+    with open(base_dir / target_csv, "r", encoding='utf-8-sig', errors='ignore') as file:
         csvfile = csv.reader(file)
+
         for row in csvfile:
             if not row[0].isnumeric():
                 info[row[0]] = row[1]
             else:
                 items.append(row[0])
                 desc.append(row[1])
+
+        if not items:
+            raise ValueError("Error: No items found in the CSV file.")
+        
+        if not desc:
+            raise ValueError("Error: No descriptions found in the CSV file.")
+        
+        if not info:
+            raise ValueError("Error: No information found in the CSV file.")
+        
     return items, desc, info
 
 def create_doc_context(base_dir: Path):
-    assert (base_dir / 'points.csv').exists(), "points.csv does not exist" # Check if the file exists
+    assert (base_dir / target_csv).exists(), "points.csv does not exist" # Check if the file exists
     frameworks, framework = [], []
-    csv_path = base_dir / 'points.csv'
+    csv_path = base_dir / target_csv
     items, desc, info = read_csv(base_dir)
 
     for i, d in zip(items, desc):
@@ -47,13 +64,13 @@ def create_doc_context(base_dir: Path):
     return frameworks, info
 
 def combine_docs(base_dir: Path, num_pages: int):
-    assert (base_dir / 'TnC forms').exists(), "TnC forms directory does not exist" # Check if the directory exists
-    master_doc_path = base_dir / 'TnC forms' / 'TnC form0.docx'
+    assert (base_dir / target_dir).exists(), "TnC forms directory does not exist" # Check if the directory exists
+    master_doc_path = base_dir / target_dir / 'TnC form0.docx'
     master = Document(master_doc_path)
     composer = Composer(master)
     
     for page in range(num_pages + 1): # Loop through the pages
-        temp_doc_path = base_dir / 'TnC forms' / f'TnC form{page}.docx' # Path to the temporary document
+        temp_doc_path = base_dir / target_dir / f'TnC form{page}.docx' # Path to the temporary document
         if temp_doc_path.exists(): # Check if the file exists and it's not the master document
             if page > 0:
                 temp_doc = Document(temp_doc_path)
@@ -66,27 +83,29 @@ def combine_docs(base_dir: Path, num_pages: int):
 
         
 
-    final_doc_path = base_dir / 'TnC forms' / 'TnC form.docx'
+    final_doc_path = base_dir / target_dir / 'TnC form.docx'
     composer.save(final_doc_path)
 
     print(f"Succeed! Document saved to {final_doc_path}")
 
 def create_doc(base_dir: Path):
-    assert (base_dir / 'TnC template.docx').exists(), "TnC template.docx does not exist" # Check if the file exists
-    template_path = base_dir / 'TnC template.docx'
+    assert (base_dir / target_template).exists(), "TnC template.docx does not exist" # Check if the file exists
+    template_path = base_dir / target_template
     doc = DocxTemplate(template_path)
     frameworks, info = create_doc_context(base_dir)
 
-    #check if directory TnC forms exists, if not, create one
-    if not (base_dir / 'TnC forms').exists():
-        (base_dir / 'TnC forms').mkdir()
+    # Check if directory TnC forms exists, if not, create one
+    if not (base_dir / target_dir).exists():
+        print(f"Directory {target_dir} does not exist. Creating directory...")
+        (base_dir / target_dir).mkdir()
+        print(f"Directory {target_dir} created.")
 
     for page, framework in enumerate(frameworks):
         context = {'frameworks': framework} # Create a new dictionary for each page
         context = context | info  # Merge the two dictionaries
         jinja_env = jinja2.Environment(autoescape=True) # Create a Jinja environment
         doc.render(context, jinja_env) # Render the document
-        doc.save(base_dir / 'TnC forms' / f'TnC form{page}.docx') # Save the document
+        doc.save(base_dir / target_dir / f'TnC form{page}.docx') # Save the document
 
     combine_docs(base_dir, page) # Combine the documents
 
